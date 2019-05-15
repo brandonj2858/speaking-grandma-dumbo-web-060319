d5799820224c93e6e1625e53b72d9251bbd7196d
Brandon James <brandonj2858@gmail.com>
2019-05-15 17:01:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
   return "I LOVE YOU TOO PUMPKIN!"
-  if phrase = upcase 
+  if phrase = phrase.downcase 
     return "HUH?! SPEAK UP, SONNY!"
 end
